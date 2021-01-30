379155edce3608ef0b578822d751aa5e82bcb90c
ajpadilla88 <apadilla@theegg.io>
2021-01-30 19:27:04 -0400

    Modificando metodo show y process

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -3,12 +3,16 @@
 namespace App\Http\Controllers;
 
 use App\Models\Order;
+use App\Models\PaymentAttempt;
 use App\Repositories\OrderRepository;
 use App\Repositories\PaymentAttemptRepository;
 use App\Services\Payment\PaymentService;
 use App\Services\Placetopay\WebCheckout\PlacetopayWebCheckoutService;
+use Exception;
+use GuzzleHttp\Exception\GuzzleException;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class PaymentController extends Controller
 {
@@ -46,25 +50,89 @@ class PaymentController extends Controller
 
     public function show()
     {
-        return view('layouts.orders.pay');
-    }
-
-    public function process()
-    {
         /** @var Order $order */
         $order = $this->orderRepository->getByUserId(Auth::user()->id);
 
-        $data = $this->paymentService->generateRequestData($order);
+        return view('layouts.orders.pay', compact('order'));
+    }
 
-        $response = $this->placetopayWebCheckoutService->createRequest($data);
+    public function process(Request $request)
+    {
 
-        $paymentAttemp = $this->paymentAttemptRepository->create([
-            'external_id',
-            'url_process',
-            'state' => 'INITIAL',
-            'order_id' => $order->id
-        ]);
+        try {
+            DB::beginTransaction();
+            /** @var Order $order */
+            $order = $this->orderRepository->getByUserId(Auth::user()->id);
 
-        //Redirecionar hacia placetopay
+            /** @var PaymentAttempt $paymentAttemp */
+            $paymentAttempt = $order->getFirstPaymentAttempt();
+
+            $data = $this->paymentService->generateRequestData($order);
+
+            $responseCreateRequest = $this->placetopayWebCheckoutService->createRequest($data);
+            $responseGetRequestInformation = $this->placetopayWebCheckoutService->getRequestInformation($responseCreateRequest->requestId);
+
+            $this->paymentAttemptRepository->update($paymentAttempt,[
+                'external_id' => $responseCreateRequest->requestId,
+                'url_process' => $responseCreateRequest->processUrl,
+                'state' => $responseGetRequestInformation->status->status,
+                'order_id' => $order->id
+            ]);
+            DB::commit();
+
+            return redirect()->to($responseCreateRequest->processUrl);
+
+        } catch (Exception $exception) {
+            DB::rollBack();
+
+            return redirect()->route('pay_order')->withErrors(["order_error"=>"{$exception->getMessage()}"]);
+        } catch (GuzzleException $exception) {
+            return redirect()->route('pay_order')->withErrors(["order_error"=>"{$exception->getMessage()}"]);
+        }
+
+    }
+
+    public function updateOrderState(Request $request)
+    {
+        try {
+            DB::beginTransaction();
+
+            /** @var Order $order */
+            $order = $this->orderRepository->getByUserId(Auth::user()->id);
+
+            /** @var PaymentAttempt $paymentAttemp */
+            $paymentAttempt = $order->getFirstPaymentAttempt();
+
+            $responseGetRequestInformation = $this->placetopayWebCheckoutService->getRequestInformation($paymentAttempt->external_id);
+
+            $this->paymentAttemptRepository->update($paymentAttempt,[
+                'external_id' => $responseGetRequestInformation->requestId,
+                'state' => $responseGetRequestInformation->status->status,
+                'order_id' => $order->id
+            ]);
+
+            $status = $responseGetRequestInformation->status->status;
+
+            if($status == 'APPROVED'){
+                $this->orderRepository->update($order, ['status' => 'PAYED']);
+            }
+
+            if($status == 'REJECTED' || $status == 'FAILED'){
+                $this->orderRepository->update($order, ['status' => 'REJECTED']);
+            }
+
+            DB::commit();
+
+            return redirect()->route('pay_order');
+
+        } catch (Exception $exception) {
+            DB::rollBack();
+
+            return redirect()->route('pay_order')->withErrors(["order_error"=>"{$exception->getMessage()}"]);
+
+
+        } catch (GuzzleException $exception) {
+            return redirect()->route('pay_order')->withErrors(["order_error"=>"{$exception->getMessage()}"]);
+        }
     }
 }
