5fa9deaa34f69a4ef9393612f8febdde1ec77d79
ajpadilla88 <apadilla@theegg.io>
2021-01-30 19:18:15 -0400

    Agregando nuevo campo al formulario

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -47,8 +47,14 @@
                     @csrf
                     <div style="margin-bottom: 25px" class="input-group">
                         <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
-                        <input id="login-username" type="text" class="form-control" name="fullname" value=""
-                               placeholder="fullname">
+                        <input id="login-username" type="text" class="form-control" name="first_name" value=""
+                               placeholder="first name">
+                    </div>
+
+                    <div style="margin-bottom: 25px" class="input-group">
+                        <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
+                        <input id="login-username" type="text" class="form-control" name="last_name" value=""
+                               placeholder="last name">
                     </div>
 
                     <div style="margin-bottom: 25px" class="input-group">
