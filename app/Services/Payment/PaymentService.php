1558b6d775fb824509f00a831359c77968cf4c0b
ajpadilla88 <apadilla@theegg.io>
2021-01-30 19:35:09 -0400

    Agregando datos de la orden, para realizar request

--- app/Services/Payment/PaymentService.php
+++ app/Services/Payment/PaymentService.php
@@ -5,31 +5,36 @@ namespace App\Services\Payment;
 
 
 use App\Models\Order;
+use Carbon\Carbon;
 
 class PaymentService
 {
     public function generateRequestData(Order $order)
     {
+
+        $reference = 'TEST_' . time();
+        $expiration_date = Carbon::now()->addDays(1)->format('c');
+
         $data = [
             "buyer" => [
-                "name" => "River",
-                "surname" => "Dickens",
-                "email" => "dnetix@yopmail.com",
-                "document" => "1040035000",
-                "documentType" => "CC",
-                "mobile" => 3006108300
+                "name" => "{$order->customer_name}",
+                "surname" => "{$order->customer_last_name}",
+                "email" => "{$order->customer_email}",
+                "document" => "{$order->customer_document_number}",
+                "documentType" => "{$order->customer_document_type}",
+                "mobile" => $order->customer_mobile
             ],
             "payment" => [
-                "reference" => "TEST_20210126_165513",
+                "reference" => "{$reference}",
                 "description" => "Animi hic hic voluptas.",
                 "amount" => [
                     "currency" => "COP",
-                    "total" => 149000
+                    "total" => $order->amount
                 ]
             ],
-            "expiration" => "2021-01-27T17:55:13-05:00",
+            "expiration" => "{$expiration_date}",
             "ipAddress" => "127.0.0.1",
-            "returnUrl" => "http://localhost:8089/show",
+            "returnUrl" => "http://localhost:8089/update/order/state",
             "userAgent" => "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36",
             "paymentMethod" => null,
         ];
